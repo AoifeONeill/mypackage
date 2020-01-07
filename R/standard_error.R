8df45c56e7fb2483adf44d6f7a78d87f715fe26d
AoifeONeill <aoneill570@gmail.com>
2020-01-07 13:38:04 +0000

    feat adding new function to calculate standard error

--- R/standard_error.R
+++ R/standard_error.R
@@ -1,3 +1,14 @@
+#' Calculate the standard error for a sample
+#'
+#' returns the standard error of
+#' a numeric vector
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
 standard_error <- function(v) {
   height/sqrt(length(v))
 }
