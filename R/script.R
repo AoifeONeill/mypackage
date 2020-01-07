c58362f2366df9f8ee15caafb2f297d6f2003641
AoifeONeill <aoneill570@gmail.com>
2020-01-07 12:02:59 +0000

    package and function made

---
+++ R/script.R
@@ -0,0 +1,32 @@
+getwd()
+usethis::use_git()
+library(usethis)
+library(devtools)
+library(tidyverse)
+library(pkgdown)
+library(testthat)
+library(roxygen2)
+
+
+height <- rnorm(100, mean = 10, sd = 0.8) %>%
+  round(4)
+
+sum((height - mean(height))^2)
+
+sum_squares <- function(v) {
+  sum((height - mean(height))^2)
+}
+
+sum_squares (rnorm(10))
+
+use_r('sum_squares')
+
+#devtools::load_all()
+#getwd
+#load_all(reset=FALSE)
+
+?use_mit_license()
+use_mit_license("Aoife O'Neill")
+
+
+use_github()
